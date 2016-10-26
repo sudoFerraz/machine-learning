ebdca17b013d6f5fb49b324b6b761ccd24f0251b
root <ferraz@ufu.br>
2016-10-26 00:34:57 +0000

    Tree decision algorithm for gender classification

---
+++ gender_classification/gender_classification.py
@@ -0,0 +1,17 @@
+from sklearn import tree
+
+# [altura, peso, tamanho do pe]
+x = [[181 ,80 ,44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [164, 60, 38]\
+    , [166, 65, 40], [190, 90, 47], [175, 64, 39], [177, 70, 40], [159, 75, 42\
+    ], [181, 85, 43]]
+
+y = ['male', 'female', 'female', 'female', 'male', 'male', 'male', 'female', '\
+    male', 'female', 'male']
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(x,y)
+
+prediction = clf.predict([[190, 70, 43]])
+
+print prediction
